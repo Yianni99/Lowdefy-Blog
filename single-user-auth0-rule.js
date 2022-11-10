5e07cdb4e552f0cf6e88b2ba7984535f86ceacb5
Yianni99 <yianni99y0@gmail.com>
2022-11-10 02:45:57 +0200

    Added searching

--- single-user-auth0-rule.js
+++ single-user-auth0-rule.js
@@ -3,6 +3,6 @@ function emailDomainWhitelist(user, context, callback) {
 	if (user.email !== configuration.ALLOWED_USER_EMAIL) {
 		return callback(new UnauthorizedError('Access denied.'));
 	}
-  
+
 	return callback(null, user, context);
 }
